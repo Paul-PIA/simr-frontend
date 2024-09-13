881935ebdb021667de0d19879a19fbc251e6100f
NathanOik <Nathan.Oiknine@parisinfrastructureadvisory.com>
2024-09-13 16:41:04 +0200

    Ajout d'un package

--- contents/Grid.js
+++ contents/Grid.js
@@ -3,6 +3,7 @@ import * as XLSX from "xlsx"; //
 import { AgGridReact } from "ag-grid-react"; // React Data Grid Component
 import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the Data Grid
 import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the Data Grid
+import "ag-grid-enterprise";
 
 export function Grid() {
   // Row Data: The data to be displayed.
