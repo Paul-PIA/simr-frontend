2f105f47db6be23d19288ce09790ab8a709b2ca5
NathanOik <Nathan.Oiknine@parisinfrastructureadvisory.com>
2024-10-10 11:07:24 +0200

    Amélioration de SideBar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
 import { Layout, Menu } from "antd";
-import { BookOutlined, TeamOutlined } from "@ant-design/icons";
+import { ApiOutlined, BookOutlined, TeamOutlined } from "@ant-design/icons";
 import { jwtDecode } from "jwt-decode";
 import { apiClient } from "../services/api";
 
@@ -11,25 +11,52 @@ const { SubMenu } = Menu;
 export default function _Sider() {
   const [collapsed, setCollapsed] = useState(false); // open and close the sider
   const [openKeys, setOpenKeys] = useState([]); //open and close subMenu
-  const [contracts,setContracts]=useState([])
+  const [contracts,setContracts]=useState([]);
+  const [exercices,setExercices]=useState([]);
+  const [files,setFiles]=useState([]);
+  const [access,setAccess]=useState([]);
 
-  const fetchContracts=async ()=>{
+  const fetchContractsAndMore=async ()=>{
     const token=localStorage.getItem('access');
     const decoded=jwtDecode(token);
     const id=decoded.user_id;
     const org=await apiClient({
       method:'GET',
-      path:`user/${id}/`,
-      data:{}
+      path:`user/${id}/`
     }).org;
     const con=await apiClient({
       method:'GET',
       path:`contract/?org_icontains=${org}`
     });
     setContracts(con);
+    const exercises=await Promise.all(con.map(
+      async (con)=>await apiClient({
+        method:'GET',
+        path:`exercise/?con=${con.id}`
+      })
+    ));
+    const ex=exercises.reduce((pre, cur) =>
+      pre.concat(cur));
+    console.log(ex);
+    setExercices(ex);
+    const fich=await Promise.all(ex.map(
+      async (exer)=>await apiClient({
+        method:'GET',
+        path:`file/?exer=${exer.id}`
+      })
+    ));
+    const fichiers=fich.reduce((pre, cur) =>
+      pre.concat(cur));
+const droits=await Promise.all(fichiers.map(
+  async (file)=>await apiClient({
+    method:'GET',
+    path:`access/${file.id}/`
+  })
+));
+setFiles(fichiers.filter((file,index)=>file.is_public || droits[index].user.includes(id) || droits[index].org.includes(org)))
   };
 
-  useEffect(()=>{fetchContracts()},[]);
+  useEffect(()=>{fetchContractsAndMore()},[]);
   const handleOpenChange = (keys) => {
     const latestOpenKey = keys.find((key) => !openKeys.includes(key));
     if (!latestOpenKey) {
@@ -54,7 +81,7 @@ export default function _Sider() {
       >
         <SubMenu
           key="sub1"
-          title={collapsed ? null : "Contracts"}
+          title={collapsed ? null : "Mes Contracts"}
           icon={<BookOutlined />}
         >
           {contracts.map((con,index)=>(
@@ -65,11 +92,25 @@ export default function _Sider() {
         </SubMenu>
         <SubMenu
           key="sub2"
-          title={collapsed ? null : "My Firm"}
-          icon={<TeamOutlined />}
+          title={collapsed ? null : "Mes Exercices"}
+          icon={<BookOutlined />}
         >
-          <Menu.Item key="1">Fellow</Menu.Item>
-          <Menu.Item key="2">Contact</Menu.Item>
+          {exercices.map((exer,index)=>(
+          <Menu.Item key={index+1} 
+          onClick={()=>{window.location=`/exercise?id=${exer.id}`}}>
+            {exer.name}</Menu.Item>
+          ))}
+        </SubMenu>
+        <SubMenu
+          key="sub3"
+          title={collapsed ? null : "Mes Fichiers"}
+          icon={<BookOutlined />}
+        >
+          {files.map((file,index)=>(
+          <Menu.Item key={index+1} 
+          onClick={()=>{window.location=`/file?id=${file.id}`}}>
+            {file.name}</Menu.Item>
+          ))}
         </SubMenu>
       </Menu>
     </Sider>
