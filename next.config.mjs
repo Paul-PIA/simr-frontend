8c22f15805f4077e7c526ec5b63bcd7639a7d565
NathanOik <Nathan.Oiknine@parisinfrastructureadvisory.com>
2024-10-28 09:21:30 +0100

    Nouvelle tentative de correction

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,12 @@
 const nextConfig = {async rewrites() {
     return [
       {
-        source: '/api/:path*/',
-        destination: 'https://simr-xxm0.onrender.com/api/:path*/',
+        source: '/api/:path1/?:path2*',
+        destination: 'https://simr-xxm0.onrender.com/api/:path1/?:path2*',
       },
       {
         source: '/api/:path*',
-        destination: 'https://simr-xxm0.onrender.com/api/:path*',
+        destination: 'https://simr-xxm0.onrender.com/api/:path*/',
       }
     ]
   }};
