6b6543a1852965f6e4e5f2e2a91fe5ac9fc5e0ec
NathanOik <Nathan.Oiknine@parisinfrastructureadvisory.com>
2024-10-29 10:00:31 +0100

    Correction du cookie 2

--- services/jwt.js
+++ services/jwt.js
@@ -22,7 +22,7 @@ function getCookie(name) {
 const fetchCsrfToken = async () => {
     try {
         // On peut effectuer un appel pour s'assurer que le token est bien envoyé
-        await axios.get(document.domain+'/set-csrf-token', { withCredentials: true });
+        await axios.get(window.location.origin+'/set-csrf-token', { withCredentials: true });
         
         // Récupère le token CSRF à partir du cookie
         const csrfToken = getCookie('csrftoken');
