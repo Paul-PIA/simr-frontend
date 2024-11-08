cb642170a9faa743bc732bd8ea684858351ed1ac
NathanOik <Nathan.Oiknine@parisinfrastructureadvisory.com>
2024-11-08 14:49:31 +0100

    Amélioration du passage de la souris

--- components/HomePageButton.js
+++ components/HomePageButton.js
@@ -1,26 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 const HomePageButton = ({st}) => {
-
+  const [mouse,setMouse]=useState(false)
 
   return (
-    <a
-      href={typeof window!=='undefined'? window.location.origin:'/'}>
-    <button
-      style={Object.assign({},st,{
-        
-        top: '20px',
-        left: '10px',
-        padding: '10px 15px',
-        backgroundColor: '#007BFF',
-        color: '#fff',
-        border: 'none',
-        borderRadius: '5px',
-        cursor: 'pointer'
-      })}
-    >
-      
-      Accueil
-    </button> </a>
+      <a
+      href={typeof window !== 'undefined' ? window.location.origin : '/'}>
+      <button className="button"
+        style={Object.assign({}, st, {
+          top: '20px',
+          left: '10px',
+          padding: '10px 15px',
+          backgroundColor: mouse?'#0056b3':'#007BFF',
+          color: '#fff',
+          border: 'none',
+          borderRadius: '5px',
+          cursor: 'pointer'
+        })}
+        onMouseEnter={()=>setMouse(true)}
+        onMouseLeave={()=>setMouse(false)}
+      >
+
+        Accueil
+      </button> </a>
   );
 };
 
