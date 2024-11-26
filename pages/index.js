39fe4567aafc2510a4a2880d529b84441e94c004
NathanOik <Nathan.Oiknine@parisinfrastructureadvisory.com>
2024-11-26 12:12:10 +0100

    Ajout de la fonction onSuccess

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,14 @@
 import React,{useEffect, useState} from "react";
 import Layout from "../components/Layout";
-import { apiClient } from "../services/api";
+import { apiRefresh } from "../services/api";
 
 
 export default function Home() {
   const [connecte,setConnecte]=useState(false);
   async function test() {
-    
-  
-  try{ //On vérifie que l'utilisateur est connecté. Si ce n'est pas le cas (ou que la connection a expiré), on le redirige vers la page d'authentification
-    const response=await apiClient({
-      method:'POST',
-      path:'token/refresh/',
-      data:{refresh:localStorage.getItem('refresh')}
-    });
-    localStorage.setItem('access',response.access);
-    setConnecte(true)
-  }
-  catch (error){
-    window.location='./auth';
-  }}
+  await apiRefresh({onSuccess:()=>setConnecte(true)}) }
   useEffect(() => {
-    if (typeof window !== 'undefined') {
       test(); 
-    }
   }, []);
   if (connecte){
   return <Layout />;
--- services/api.js
+++ services/api.js
@@ -5,7 +5,7 @@ import { csrfToken } from './jwt';
 const LOCAL_URL = 'http://127.0.0.1:8000/api';
 const SERVER_URL = '/api'; // fill in this url with your adress of backend server 
 
-const API_URL = SERVER_URL; // switch LOCAL_URL or SERVER_URL to adapt the environment 
+const API_URL = LOCAL_URL; // switch LOCAL_URL or SERVER_URL to adapt the environment 
 
 axios.defaults.withCredentials = true;
 
@@ -71,14 +71,17 @@ export const apiClientGetFile = async ({ path}) => { //A utiliser pour récupér
     }
 };
 
-export const apiRefresh=async()=>{ //A utiliser pour refresh le token d'accès
+export const apiRefresh=async({onSuccess})=>{ //A utiliser pour refresh le token d'accès
     try {
         const response=await apiClient({
             method:'POST',
             path:'token/refresh/',
             data:{refresh:localStorage.refresh}
         });
-        localStorage.setItem('access',response.access)
+        localStorage.setItem('access',response.access);
+        if (onSuccess){
+            onSuccess()
+        }
     }
     catch(error){
         window.location='/auth'
