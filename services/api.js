3ccf00bcc174a8a11ac60dff15bb07cebbdcc98a
NathanOik <Nathan.Oiknine@parisinfrastructureadvisory.com>
2024-12-04 12:15:25 +0100

    Correction de l'adresse du back

--- services/api.js
+++ services/api.js
@@ -5,7 +5,7 @@ import { csrfToken } from './jwt';
 const LOCAL_URL = 'http://127.0.0.1:8000/api';
 const SERVER_URL = '/api'; // fill in this url with your adress of backend server 
 
-const API_URL = LOCAL_URL; // switch LOCAL_URL or SERVER_URL to adapt the environment 
+const API_URL = SERVER_URL; // switch LOCAL_URL or SERVER_URL to adapt the environment 
 
 axios.defaults.withCredentials = true;
 
