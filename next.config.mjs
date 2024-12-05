3d2760b425d01b788479573f6edfe55934d50638
NathanOik <Nathan.Oiknine@parisinfrastructureadvisory.com>
2024-12-05 10:08:39 +0100

    Mise à jour des packages et correction

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {trailingSlash: true,
   output: 'export',
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*/',
-        destination: 'https://simr-yo8m.onrender.com/api/:path*/',
-      },
-      {source:'/set-csrf-token/',
-      destination:'https://simr-yo8m.onrender.com/set-csrf-token'
-      },
-      {source:'/media/:path*',
-        destination:'https://simr-yo8m.onrender.com/media/:path*'
-      }
-    ]
-  }};
+  // async rewrites() { //A décommenter pour faire tourner en web service
+  //   return [
+  //     {
+  //       source: '/api/:path*/',
+  //       destination: 'https://simr-yo8m.onrender.com/api/:path*/',
+  //     },
+  //     {source:'/set-csrf-token/',
+  //     destination:'https://simr-yo8m.onrender.com/set-csrf-token'
+  //     },
+  //     {source:'/media/:path*',
+  //       destination:'https://simr-yo8m.onrender.com/media/:path*'
+  //     }
+  //   ]
+  // }
+};
 
 export default nextConfig;
